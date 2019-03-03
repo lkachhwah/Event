41308c56fcd82265634e4c12fc808de2a6cb96a4
Lalitkkachhwah47@gmail.com <Lalitkkachhwah47@gmail.com>
2019-03-03 14:42:48 +0530

    added page length

--- src/main/java/com/example/event/util/Constant.java
+++ src/main/java/com/example/event/util/Constant.java
@@ -3,7 +3,7 @@ package com.example.event.util;
 public class Constant {
 	public final static String BASE_URL = "http://london.eventful.com";
 	public final static String EVENTS_CATEGORY = "/json/v2/tools/events/faceted_search?";
-	public static final String FACLET_QUERY =  "type=asynch&location_type=metro_id&location_id=67&page_size=10";
+	public static final String FACLET_QUERY =  "type=asynch&location_type=metro_id&location_id=67&page_size=1000";
 	
 	private Constant(){
 		
